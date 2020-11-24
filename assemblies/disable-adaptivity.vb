2f7f6cefcc4ae2d78f4461c41eceb6c935dd4c6d
jordanrobot <meloncholia@gmail.com>
2020-11-24 12:23:27 -0500

    added disable-adaptivity

---
+++ assemblies/disable-adaptivity.vb
@@ -0,0 +1,28 @@
+'#############################
+'###   Disable Adaptivity  ###
+'#############################
+
+' Removes all adaptivity for components in this assembly.
+' This is a sledgehammer, use wisely.
+
+Dim doc As Document = ThisApplication.ActiveDocument
+
+If doc.DocumentType <> kAssemblyDocumentObject Then
+	MessageBox.Show("Please run this rule in an assembly file.  Exiting...", "Disable Adaptivity")
+	Return
+End If
+
+
+Dim i As Integer = 0
+
+For Each componentDoc As Document In doc.AllReferencedDocuments
+	Try
+		If componentDoc.ModelingSettings.AdaptivelyUsedInAssembly = True Then
+			componentDoc.ModelingSettings.AdaptivelyUsedInAssembly = False
+			i += 1
+		End If
+	Catch
+	End Try
+Next
+
+Logger.Info("disable-adaptivity.vb: Edited " & i & " file(s).")
