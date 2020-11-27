8cb79e2de1c37c94e912966403d87b1e4dea344c
nannerdw <nannerdw@users.noreply.github.com>
2020-11-27 10:16:44 -0600

    Added scripts to set "Checked By" and "Approved By" iProps

---
+++ iproperties/set-eng-approved-by.vb
@@ -0,0 +1,64 @@
+﻿'Author: nannerdw
+'
+'Last Modified Date: 27 Nov, 2020
+'
+'Description: Fills out the "Eng. Approved By" and "Eng. Approved Date" iProperties,
+'	and displays the results in the iLogic log window
+'
+'Note: User name can be set in Application Options - General
+'
+'Optional RuleArguments:
+'	doc As Document: The document whose iProps will be changed.
+'	(The document that called this rule will be used if no doc is passed to this rule.)
+
+Option Explicit On
+
+Private Class RuleMain
+	Private _doc As Document = Nothing
+	
+	Private Sub Main
+		_doc = RuleArguments("doc")
+		If _doc Is Nothing Then _doc = ThisDoc.Document
+			
+		Dim docName As String
+		If _doc.FileSaveCounter > 0 Then
+			docName = System.IO.Path.GetFileName(_doc.FullFileName)
+		Else
+			docName = _doc.Displayname
+		End If
+		
+		Logger.Info(docName)
+		
+		If Not _doc.IsModifiable Then
+			'A generated iPart or iAssembly member file is an example of a file that is not modifiable
+			Logger.Error("iProps can't be updated because document is not modifiable")
+			Exit Sub
+		End If
+		
+		If System.IO.File.GetAttributes(_doc.FullFileName) And System.IO.FileAttributes.ReadOnly Then
+			Logger.Error("iProps can't be updated because file is read-only")
+			Exit Sub
+		End If
+		
+		Me.UpdateProp("Engr Approved By", ThisApplication.UserName)
+		Me.UpdateProp("Engr Date Approved", DateTime.Today())
+		
+		ThisApplication.UserInterfaceManager.DockableWindows("ilogic.logwindow").Visible = True
+	End Sub
+	
+	Private Sub UpdateProp(propName As String, newValue As Object)
+		Dim prop As Inventor.Property = _doc.PropertySets("Design Tracking Properties").Item(propName)
+		
+		If prop.Expression <> newValue Then
+			Try
+				prop.Expression = newValue
+			Catch ex As Exception
+				Logger.Error(vbTab & """" & propName & """ iProp could not be modified")
+				Exit Sub
+			End Try
+			Logger.Info(vbTab & """" & propName & """ iProp updated")
+		Else
+			Logger.Info(vbTab & """" & propName & """ iProp is already up-to-date")
+		End If
+	End Sub
+End Class
