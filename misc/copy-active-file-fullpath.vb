011fdacc9c9aac3b488dc17ac5751e6b7fd026c9
nannerdw <nannerdw@users.noreply.github.com>
2020-11-27 15:38:43 -0600

    Added filename copy scripts

---
+++ misc/copy-active-file-fullpath.vb
@@ -0,0 +1,11 @@
+﻿'Author: nannerdw
+'Last Modified Date:  27 Nov, 2020
+'Description: Copies the active document's fullFileName to the clipboard
+
+Dim doc As Document = ThisApplication.ActiveDocument
+
+If doc.FileSaveCounter = 0 Then
+	MessageBox.Show("The active document must be saved first.")
+Else
+	My.Computer.Clipboard.SetText(doc.FullFileName)
+End If
