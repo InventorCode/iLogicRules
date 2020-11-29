1f6c8831bd7ace98dba7b1275b447e55b8173e7f
nannerdw <nannerdw@users.noreply.github.com>
2020-11-29 08:12:24 -0600

    added find-file snippets

---
+++ snippets/find-file-in-ilogic-rule-dirs.vb
@@ -0,0 +1,12 @@
+﻿'Source: https://github.com/InventorCode/iLogicRules
+'Author: nannerdw
+
+Private Function FindFileInIlogicRuleDirectories(fileName As String) As String
+'Returns the first matching file found in the iLogic external rule directories
+'fileName can contain wilcard characters * and ?
+'Returns an empty string if nothing was found
+	For Each ruleDir As String In iLogicVb.Automation.FileOptions.ExternalRuleDirectories()
+		Dim files As String() = System.IO.Directory.GetFiles(ruleDir, fileName, System.IO.SearchOption.AllDirectories)
+		If files.Count > 0 Then Return files(0)
+	Next
+End Function
