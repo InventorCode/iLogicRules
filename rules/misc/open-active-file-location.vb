0aee4ceea1e29ef578539356aee59566638ae316
nannerdw <nannerdw@users.noreply.github.com>
2020-11-29 10:50:14 -0600

    added scripts for opening folders

---
+++ rules/misc/open-active-file-location.vb
@@ -0,0 +1,12 @@
+﻿'Source: https://github.com/InventorCode/iLogicRules
+'Title: Open Active File Location
+'Author: nannerdw
+'Description: Opens the active edit document's file location
+
+Dim doc As Document = ThisApplication.ActiveEditDocument
+
+If doc.FileSaveCounter = 0 Then
+	MessageBox.Show("The active edit document has not been saved.", iLogicVb.RuleName)
+Else
+	Process.Start(System.IO.Path.GetDirectoryName(doc.FullFileName))
+End If
