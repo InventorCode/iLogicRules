c8bfd6d1d1bab9f2e61a89f3bdde786413157459
nannerdw <nannerdw@users.noreply.github.com>
2020-11-30 16:08:39 -0600

    Update get-selected-by-type.vb
    replaced with two overloaded functions

--- snippets/get-selected-by-type.vb
+++ snippets/get-selected-by-type.vb
@@ -1,8 +1,8 @@
 ﻿'Source: https://github.com/InventorCode/iLogicRules
 'Title: Get Selected By Type
 'Author: nannerdw
-'Description: Returns a list of pre-selected objects matching one of the input types.
-'objTypes can be a single ObjectTypeEnum or an object implementing IEnumerable(Of ObjectTypeEnum)
+'Description: Two functions for returning a list of pre-selected objects matching either:
+'ObjectTypeEnum or IEnumerable(Of ObjectTypeEnum)
 
 'Sub Main is provided as an example for calling GetSelectedByType
 Private Sub Main
@@ -18,20 +18,19 @@ Private Sub Main
 		text:= oPoints.Count & " 2D sketch point(s) selected" & vbCrLf & 
 				oLines.Count & " sketch lines(s) selected")
 End Sub
+		
+Private Function GetSelectedByType(objTypes As IEnumerable (Of ObjectTypeEnum)) As List(Of Object)
+'Returns a list of pre-selected objects matching one of the input types
+	Return (
+		From x In ThisApplication.ActiveDocument.SelectSet
+		Where DirectCast(objTypes, IEnumerable(Of ObjectTypeEnum)).Contains(x.Type)
+		).ToList
+End Function
 
-Private Function GetSelectedByType(objTypes As Object) As List(Of Object)
-'Returns a list of pre-selected objects matching one of the input types.
-'objTypes can be a single ObjectTypeEnum or an object implementing IEnumerable(Of ObjectTypeEnum)
-	Dim selSet As SelectSet = ThisApplication.ActiveDocument.SelectSet
-	
-	If TypeOf objTypes Is ObjectTypeEnum Then
-		Return (From x In selSet Where objTypes = x.Type).ToList
-	Else If TypeOf objTypes Is IEnumerable(Of ObjectTypeEnum) Then
-		Return (
-			From x In selSet
-			Where DirectCast(objTypes, IEnumerable(Of ObjectTypeEnum)).Contains(x.Type)
-			).ToList
-	Else
-		Throw New ArgumentException
-	End If
+Private Function GetSelectedByType(objTypes As ObjectTypeEnum) As List(Of Object)
+'Returns a list of pre-selected objects matching the input type
+	Return (
+		From x In ThisApplication.ActiveDocument.SelectSet
+		Where objTypes = x.Type
+		).ToList
 End Function
