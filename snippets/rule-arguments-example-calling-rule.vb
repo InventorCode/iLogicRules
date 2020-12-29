12eea66b088cf3fa4b5756c8f2fc2ac6ba324751
nannerdw <nannerdw@users.noreply.github.com>
2020-12-29 06:06:04 -0600

    added rule argument example

---
+++ snippets/rule-arguments-example-calling-rule.vb
@@ -0,0 +1,13 @@
+﻿'Source: https://github.com/InventorCode/iLogicRules
+'Author: nannerdw
+'Description: This is an example of how to pass RuleArguments to and from an iLogic rule.
+'This rule calls rule-arguments-example.vb
+
+Dim x As Integer = CInt(InputBox(Prompt :="Enter an integer", Title :="Input"))
+
+Dim map As Inventor.NameValueMap = ThisApplication.TransientObjects.CreateNameValueMap()
+map.Add("x", x)
+
+iLogicVb.RunExternalRule("rule-arguments-example.vb", map)
+
+MessageBox.Show(map.Item("ReturnValue"))
