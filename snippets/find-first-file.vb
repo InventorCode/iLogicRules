f7d6aae6f4429744e215c3d44c3cf73b4e928eb2
Wes <nannerdw@users.noreply.github.com>
2021-01-01 23:34:11 -0600

    Update and rename find-file.vb to find-first-file.vb
    Added XML comments

---
+++ snippets/find-first-file.vb
@@ -0,0 +1,18 @@
+﻿'Source: https://github.com/InventorCode/iLogicRules
+'Author: nannerdw
+
+''' <summary>
+''' Returns the first matching file in a directory. Filename can contain wilcard characters * and ?
+''' </summary>
+''' 
+''' <param name="Recurse">Search all subfolders</param>
+''' 
+''' <returns>Full file path, or an empty string if nothing was found.</returns>
+''' 
+Private Function FindFirstFile(Filename As String, RootFolder As String, Optional Recurse As Boolean = True) As String
+	Dim files As String() = System.IO.Directory.GetFiles(
+		path :=RootFolder, 
+		searchPattern :=Filename, 
+		searchOption :=If (Recurse, System.IO.SearchOption.AllDirectories, System.IO.SearchOption.TopDirectoryOnly))
+	If files.Count > 0 Then Return files(0)
+End Function
